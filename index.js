a2ff54ede3966eb462a29b2001b7059294b95bf0
lusyapapoyan <you@example.com>
2018-12-08 16:45:01 +0400

    Lesson 3 files

--- index.js
+++ index.js
@@ -26,18 +26,85 @@
 //     console.log("Example is running on port 3000");
 // });
 
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-app.get("/", function(req, res){
-    res.redirect('http://google.com');
-});
+// app.use(express.static("public"));
 
-app.get("/google/:search", function(req,res){
-    var n = req.params.search;
-    res.send(n);
-});
+// app.get("/", function(req,res){
+//     res.redirect("index.html");
+// });
 
-app.listen(3000,function(){
-    console.log("Example is running on port 3000");
-});
+// app.get("/", function (req, res) {
+//     res.send("<h1>Hello world</h1>");
+// });
+
+// app.get("/google/:search", function (req, res) {
+//     var search = req.params.search;
+//     res.redirect('http://google.com/search?q=' + search);
+// });
+
+// app.get("/*", function (req, res) {
+//     res.status(404).send("Error");
+// })
+
+// app.listen(3000, function () {
+//     console.log("Example is running on port 3000");
+// });
+
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+
+// function main(){
+//     console.log(mySquareObject.getArea())
+// }
+
+// main();
+var fs = require('fs');
+
+function main() {
+    var file = "hello.txt";
+    fs.appendFileSync(file, "Hello world\n");
+}
+main();
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "Hello world\n", function(err){
+//        console.log("fs.writeFile ended");
+//    });
+//    console.log("fs.writeFile");
+// }
+// main();
+// var fs = require('fs');
+
+// function main() {
+//    fs.writeFileSync("hello.txt", "Hello world\n");
+//    console.log("fs.writeFile");
+// }
+// main();
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//    fs.writeFileSync("dummytext.txt", dummyText);
+//    var text = fs.readFileSync("dummytext.txt").toString();
+//    console.log(dummyText == text);
+//    console.log(text);
+//    fs.writeFileSync("undummytext.txt",
+//        text.replace("Apple", "Microsoft")
+//    );
+// }
+// main();
+var obj =
+{
+    "first_name":"Vardan",
+    "last_name":"Hovsepyan",
+    "age":13,
+    "tumo_student":true
+
+}
+var myJSON = JSON.stringify(obj);
+console.log(myJSON);
